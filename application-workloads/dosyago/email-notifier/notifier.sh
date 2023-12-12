ce514d4371c2505e060a7cef9cc9a9f57e28edae
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-12-12 16:52:01 +0800

    Change notifier

--- application-workloads/dosyago/email-notifier/notifier.sh
+++ application-workloads/dosyago/email-notifier/notifier.sh
@@ -2,27 +2,33 @@
 
 appInsightsKey="$1"
 
-# Function to send data to Application Insights
-sendMetric() {
-  local message="The time is now $(date)"
-  curl -X POST -H "Content-Type: application/json" -d "{
-    \"data\": {
-      \"baseData\": {
-        \"metric\": {
-          \"name\": \"CustomMetric\",
-          \"value\": 1,
-          \"properties\": {
-            \"message\": \"$message\"
+# Run sendMetric in background after a delay using nohup
+nohup bash -c '
+  # Function to send data to Application Insights
+  sendMetric() {
+    local message="The time is now $(date)"
+    curl -X POST -H "Content-Type: application/json" -d "{
+      \"data\": {
+        \"baseData\": {
+          \"metric\": {
+            \"name\": \"CustomMetric\",
+            \"value\": 1,
+            \"properties\": {
+              \"message\": \"'"$message"'\"
+            }
           }
         }
       }
-    }
-  }" "https://dc.services.visualstudio.com/v2/track" -H "x-api-key: $appInsightsKey"
-}
+    }" "https://dc.services.visualstudio.com/v2/track" -H "x-api-key: '"$appInsightsKey"'"
+  }
 
-# Wait for 15 seconds
-sleep 15
+  # Wait for 15 seconds
+  sleep 15
 
-# Send the metric
-sendMetric
+  # Send the metric
+  sendMetric
+' &> /dev/null &
+
+# Exit script immediately with status 0
+exit 0
 
