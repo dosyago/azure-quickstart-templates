41136cc1693691b26c8058766d5de304c70496c8
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-12-14 16:00:14 +0800

    ok

--- application-workloads/dosyago/email-notifier/notifier.sh
+++ application-workloads/dosyago/email-notifier/notifier.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+adminUsername=$1  # Assuming the first argument is the admin username
+
+# Outer heredoc starts here
+sudo -u "$adminUsername" bash << 'EOF'
+# Inner script starts after this line
+
 # Parameters
-region="${1//[[:space:]]/}"
-resourceId="${2//[[:space:]]/}"
-connectionString="${3//[[:space:]]/}"
+region="${2//[[:space:]]/}"
+resourceId="${3//[[:space:]]/}"
+connectionString="${4//[[:space:]]/}"
 
 # Logging
 echo "Region: [$region]"
@@ -13,12 +19,14 @@ echo "ResourceID: [$resourceId]"
 command -v jq || sudo apt install -y jq
 
 # Install Node.js using nvm
-touch ~/.bashrc
+cd $HOME
+touch $HOME/.bash_profile
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-source ~/.nvm/nvm.sh
+source $HOME/.nvm/nvm.sh
 nvm install node
 
 # Create and navigate to the application directory
+
 mkdir test-app
 cd test-app
 
@@ -32,7 +40,7 @@ npm i --save applicationinsights
 export APPINSIGHTS_CONNECTIONSTRING="${connectionString}"
 
 # Create a Node.js script using a heredoc
-cat << 'EOF' > app.js
+cat << 'INNER_EOF' > app.js
 const appInsights = require('applicationinsights');
 appInsights.setup().start();
 
@@ -45,10 +53,16 @@ const loginLinkUrl = "https://example.com/login?token=abc123";
 client.trackEvent({name: "LoginLink", properties: {url: loginLinkUrl}});
 
 console.log('Custom event sent to Application Insights');
-EOF
+INNER_EOF
 
 # Run the Node.js script
 node app.js
+
+# End of the inner script
+EOF
+# End of the outer heredoc
+
+# No commands will be run past this point as the admin user
 
 exit 0
 
