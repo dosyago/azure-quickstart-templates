eafbeece1e50ed1da83c8f228fa2db86cec08d68
Cris Stringfellow <22254235+crislin2046@users.noreply.github.com>
2023-12-16 19:05:55 +0800

    ok

--- application-workloads/dosyago/email-notifier/installBrowserBox_then_notify.sh
+++ application-workloads/dosyago/email-notifier/installBrowserBox_then_notify.sh
@@ -85,171 +85,168 @@ if ! id "$username" &>/dev/null; then
 fi
 
 # Switch to the new user and run the scripts
-su - "$username" <<BBEOF
-  cd "/home/${username}" || cd "\$HOME"
-  git clone https://github.com/BrowserBox/BrowserBox.git
-  cd BrowserBox
-  ./deploy-scripts/wait_for_hostname.sh "$HOSTNAME"
-  yes | ./deploy-scripts/global_install.sh "$HOSTNAME" "$USEREMAIL"
-  export INSTALL_DOC_VIEWER="$INSTALL_DOC_VIEWER"
-  if [[ -z "$TOKEN" ]]; then
-    setup_bbpro --port 8080 
-  else
-    setup_bbpro --port 8080 --token "$TOKEN"
-  fi
-  bbpro &>/dev/null &
+function run_heredoc_script() {
+  su - "$username" <<BBEOF
+    cd "/home/${username}" || cd "\$HOME"
+    git clone https://github.com/BrowserBox/BrowserBox.git
+    cd BrowserBox
+    ./deploy-scripts/wait_for_hostname.sh "$HOSTNAME"
+    yes | ./deploy-scripts/global_install.sh "$HOSTNAME" "$USEREMAIL"
+    export INSTALL_DOC_VIEWER="$INSTALL_DOC_VIEWER"
+    if [[ -z "$TOKEN" ]]; then
+      setup_bbpro --port 8080 
+    else
+      setup_bbpro --port 8080 --token "$TOKEN"
+    fi
+    bbpro &>/dev/null &
 
-# Inner script starts after this line
-APT=$(command -v apt-get || command -v apt || command -v dnf || command -v yum || command -v brew)
+  # Inner script starts after this line
+  APT=$(command -v apt-get || command -v apt || command -v dnf || command -v yum || command -v brew)
 
-# Parameters received from outer script
-region="$1"
-resourceId="$2"
-connectionString="$3"
-appId="$4"
-appInsightsResourceId="$5"
+  # Logging
+  echo "Region: [$region]"
+  echo "ResourceID: [$resourceId]"
+  echo "ConnectionString: [$connectionString]"
+  echo "AppInsights App ID: [$appId]"
+  echo "AppInsights Resource ID: [$appInsightsResourceId]"
 
-# Logging
-echo "Region: [$region]"
-echo "ResourceID: [$resourceId]"
-echo "ConnectionString: [$connectionString]"
-echo "AppInsights App ID: [$appId]"
-echo "AppInsights Resource ID: [$appInsightsResourceId]"
+  export DEBIAN_FRONTEND=noninteractive
+  export NEEDRESTART_SUSPEND=1
+  export NEEDRESTART_MODE=a
 
-export DEBIAN_FRONTEND=noninteractive
-export NEEDRESTART_SUSPEND=1
-export NEEDRESTART_MODE=a
+  # Install jq if not present
+  command -v jq &>/dev/null || sudo $APT install -y jq
 
-# Install jq if not present
-command -v jq &>/dev/null || sudo $APT install -y jq
+  sleep 5
 
-sleep 5
+  # Install Node.js using nvm
+  export HOME="/home/${username}"
+  export USER="${username}"
+  cd \$HOME
+  touch \$HOME/.bashrc
+  source \$HOME/.nvm/nvm.sh
+  command -v nvm || (curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash)
+  source \$HOME/.nvm/nvm.sh
+  nvm install v20 # we need v20 for the azure libraries
 
-# Install Node.js using nvm
-export HOME="/home/${username}"
-export USER="${username}"
-cd \$HOME
-touch \$HOME/.bashrc
-source \$HOME/.nvm/nvm.sh
-command -v nvm || (curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash)
-source \$HOME/.nvm/nvm.sh
-nvm install v20 # we need v20 for the azure libraries
+  # Create and navigate to the application directory
+  mkdir test-app
+  cd test-app
 
-# Create and navigate to the application directory
-mkdir test-app
-cd test-app
+  # Initialize a new Node.js project
+  npm init -y
 
-# Initialize a new Node.js project
-npm init -y
+  # Install Application Insights
+  npm i --save applicationinsights @azure/identity @azure/monitor-query bluebird
 
-# Install Application Insights
-npm i --save applicationinsights @azure/identity @azure/monitor-query bluebird
+  # Export the connection string to an environment variable
+  export APPINSIGHTS_CONNECTION_STRING="$connectionString"
+  export APPINSIGHTS_APP_ID="$appId"
+  export APPINSIGHTS_RESOURCE_ID="$appInsightsResourceId"
 
-# Export the connection string to an environment variable
-export APPINSIGHTS_CONNECTION_STRING="$connectionString"
-export APPINSIGHTS_APP_ID="$appId"
-export APPINSIGHTS_RESOURCE_ID="$appInsightsResourceId"
+  # Create a Node.js script using a heredoc
+  cat << 'INNER_EOF' > app.js
+  const appInsights = require('applicationinsights');
+  const { DefaultAzureCredential } = require('@azure/identity');
+  const { Durations, MetricsQueryClient, LogsQueryClient } = require('@azure/monitor-query');
+  console.log({LogsQueryClient});
+  const { delay } = require('bluebird');
 
-# Create a Node.js script using a heredoc
-cat << 'INNER_EOF' > app.js
-const appInsights = require('applicationinsights');
-const { DefaultAzureCredential } = require('@azure/identity');
-const { Durations, MetricsQueryClient, LogsQueryClient } = require('@azure/monitor-query');
-console.log({LogsQueryClient});
-const { delay } = require('bluebird');
+  appInsights.setup(process.env.APPINSIGHTS_CONNECTION_STRING).setSendLiveMetrics(true).start();
 
-appInsights.setup(process.env.APPINSIGHTS_CONNECTION_STRING).setSendLiveMetrics(true).start();
+  const client = appInsights.defaultClient;
 
-const client = appInsights.defaultClient;
+  // Example URL for the login link
+  const loginLinkUrl = "https://example.com/login?token=abc123";
 
-// Example URL for the login link
-const loginLinkUrl = "https://example.com/login?token=abc123";
-
-// Sending a custom event and metric
-client.trackMetric({
-  name: "LoginLink",
-  value: 1,
-  properties: { url: loginLinkUrl }  // Additional data
-});
-setTimeout(() => {
+  // Sending a custom event and metric
   client.trackMetric({
     name: "LoginLink",
     value: 1,
     properties: { url: loginLinkUrl }  // Additional data
   });
-}, 30000);
-setTimeout(() => {
-  client.trackMetric({
-    name: "LoginLink",
-    value: 1,
-    properties: { url: loginLinkUrl }  // Additional data
-  });
-}, 60000);
-setTimeout(() => {
-  client.trackMetric({
-    name: "LoginLink",
-    value: 1,
-    properties: { url: loginLinkUrl }  // Additional data
-  });
-}, 90000);
+  setTimeout(() => {
+    client.trackMetric({
+      name: "LoginLink",
+      value: 1,
+      properties: { url: loginLinkUrl }  // Additional data
+    });
+  }, 30000);
+  setTimeout(() => {
+    client.trackMetric({
+      name: "LoginLink",
+      value: 1,
+      properties: { url: loginLinkUrl }  // Additional data
+    });
+  }, 60000);
+  setTimeout(() => {
+    client.trackMetric({
+      name: "LoginLink",
+      value: 1,
+      properties: { url: loginLinkUrl }  // Additional data
+    });
+  }, 90000);
 
-console.log('Custom event sent to Application Insights');
+  console.log('Custom event sent to Application Insights');
 
-// Function to check metric availability
-async function checkMetricAvailability() {
-  const credential = new DefaultAzureCredential();
-  const monitorQueryClient = new MetricsQueryClient(credential);
-  const appId = process.env.APPINSIGHTS_APP_ID; // Set this environment variable to your Application Insights App ID
-  const appInsightsResourceId = process.env.APPINSIGHTS_RESOURCE_ID;
-  const MAX_TRIES = 150; // around about 12 and a half minutes
-  let tries = 0;
-  let code = 0;
+  // Function to check metric availability
+  async function checkMetricAvailability() {
+    const credential = new DefaultAzureCredential();
+    const monitorQueryClient = new MetricsQueryClient(credential);
+    const appId = process.env.APPINSIGHTS_APP_ID; // Set this environment variable to your Application Insights App ID
+    const appInsightsResourceId = process.env.APPINSIGHTS_RESOURCE_ID;
+    const MAX_TRIES = 150; // around about 12 and a half minutes
+    let tries = 0;
+    let code = 0;
 
-  waitMetric: while (true) {
-    tries++;
-    try {
-      const iterator = await monitorQueryClient.listMetricDefinitions(appInsightsResourceId, {metricNamespace: "azure.applicationinsights"});
-      for await ( const result of iterator ) {
-        if(result.name == "LoginLink") {
-	  console.log('Metric is available.');
-	  break waitMetric;
+    waitMetric: while (true) {
+      tries++;
+      try {
+        const iterator = await monitorQueryClient.listMetricDefinitions(appInsightsResourceId, {metricNamespace: "azure.applicationinsights"});
+        for await ( const result of iterator ) {
+          if(result.name == "LoginLink") {
+      console.log('Metric is available.');
+      break waitMetric;
+          }
         }
+          console.log('Metric not available yet, retrying in 5 seconds...');
+          if ( (tries % 5) == 0 ) {
+            console.log('5 checks without metric. Resending it...');
+            client.trackMetric({
+              name: "LoginLink",
+              value: 1,
+              properties: { url: loginLinkUrl }  // Additional data
+            });
+          } else if ( tries > MAX_TRIES ) {
+            console.error(\`Exceeded \${MAX_TRIES} checks and no metric. Quitting...\`);
+            code = 1;
+            break;
+          }
+          await delay(5000);
+      } catch (error) {
+        console.error('Error querying Application Insights:', error);
+        tries += 30; // penalize an error try
+        await delay(5000); // Retry after delay even in case of error
       }
-        console.log('Metric not available yet, retrying in 5 seconds...');
-        if ( (tries % 5) == 0 ) {
-          console.log('5 checks without metric. Resending it...');
-          client.trackMetric({
-            name: "LoginLink",
-            value: 1,
-            properties: { url: loginLinkUrl }  // Additional data
-          });
-        } else if ( tries > MAX_TRIES ) {
-          console.error(\`Exceeded \${MAX_TRIES} checks and no metric. Quitting...\`);
-          code = 1;
-          break;
-        }
-        await delay(5000);
-    } catch (error) {
-      console.error('Error querying Application Insights:', error);
-      tries += 30; // penalize an error try
-      await delay(5000); // Retry after delay even in case of error
     }
+
+    console.log('Exiting...');
+    process.exit(code);
   }
 
-  console.log('Exiting...');
-  process.exit(code);
-}
-
-// Call the function
-//checkMetricAvailability();
+  // Call the function
+  //checkMetricAvailability();
 INNER_EOF
 
-# Run the Node.js script
-(node app.js &>/dev/null &) || (echo "Node had an error" >&2 && exit 1)
-exit 0
-
-# End of the inner script
+  # Run the Node.js script
+  (node app.js &>/dev/null &) || (echo "Node had an error" >&2 && exit 1)
+  exit 0
 BBEOF
+  return 0
+}
+
 # End of the outer heredoc. Exit with the exit status of the heredoc bash script
 
-exit $?
+run_heredoc_script &
+
+exit 0
